433f976fcf3f637bc92a442266fd737c0befb1ac
Matthieu Cattin <matthieu.cattin@cern.ch>
2011-07-07 13:36:14 +0000

    Update Manifest files list.

--- rtl/Manifest.py
+++ rtl/Manifest.py
@@ -1,22 +1,12 @@
-files = ["clock_generator_ddr_s2_diff.vhd",
-				 "clock_generator_pll_s2_diff.vhd",
-				 "dma_controller.vhd",
-				 "dma_controller_wb_slave.vhd",
-				 "gn4124_core_pkg.vhd",
-				 "l2p_arbiter.vhd",
-				 "l2p_dma_master.vhd",
-				 "p2l_decode32.vhd",
-				 "p2l_dma_master.vhd",
-				 "serdes_1_to_n_clk_ddr_s2_diff.vhd",
-				 "serdes_1_to_n_clk_pll_s2_diff.vhd",
-				 "serdes_1_to_n_data_ddr_s2_se.vhd",
-				 "serdes_1_to_n_data_s2_se.vhd",
-				 "serdes_n_to_1_ddr_s2_diff.vhd",
-				 "serdes_n_to_1_ddr_s2_se.vhd",
-				 "serdes_n_to_1_s2_diff.vhd",
-				 "serdes_n_to_1_s2_se.vhd",
-				 "wbmaster32.vhd"]
-				 
+files = ["dma_controller.vhd",
+         "dma_controller_wb_slave.vhd",
+         "gn4124_core_pkg.vhd",
+         "l2p_arbiter.vhd",
+         "l2p_dma_master.vhd",
+         "p2l_decode32.vhd",
+         "p2l_dma_master.vhd",
+         "wbmaster32.vhd"]
+
 modules = { "local" : "spartan6",
-						"git" : "git://ohwr.org/hdl-core-lib/general-cores.git" }
+            "git" : "git://ohwr.org/hdl-core-lib/general-cores.git" }
 
