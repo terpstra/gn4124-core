7a14a3c20845a04ed44cb4ba542fdf796d43ceee
Matthieu Cattin <matthieu.cattin@cern.ch>
2011-07-12 06:24:03 +0000

    [BUG] Remove pkg from Manifest.

--- gn4124core/rtl/Manifest.py
+++ gn4124core/rtl/Manifest.py
@@ -1,6 +1,5 @@
 files = ["dma_controller.vhd",
          "dma_controller_wb_slave.vhd",
-         "gn4124_core_pkg.vhd",
          "l2p_arbiter.vhd",
          "l2p_dma_master.vhd",
          "p2l_decode32.vhd",
