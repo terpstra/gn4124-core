a2c1a9e5dd50f06779b192f73e87196e4ec4ba6f
Matthieu Cattin <matthieu.cattin@cern.ch>
2011-08-09 07:44:22 +0000

    Move general core lib fetch to rtl top Manifest in /spec/rtl

--- hdl/gn4124core/rtl/Manifest.py
+++ hdl/gn4124core/rtl/Manifest.py
@@ -6,8 +6,6 @@ files = ["dma_controller.vhd",
          "p2l_dma_master.vhd",
          "wbmaster32.vhd"]
 
-modules = { "local" : "spartan6",
-            "git" : "git://ohwr.org/hdl-core-lib/general-cores.git" }
+modules = { "local" : "spartan6"}
 
-fetchto = "../ip_cores"
 
--- hdl/spec/rtl/Manifest.py
+++ hdl/spec/rtl/Manifest.py
@@ -1,5 +1,7 @@
 files = ["spec_gn4124_test.vhd"]
 
 modules = {"local" : ["../../common/rtl",
-                      "../../gn4124core/rtl",
-                      "../../gn4124core/ip_cores"]}
+                      "../../gn4124core/rtl"],
+           "git" : "git://ohwr.org/hdl-core-lib/general-cores.git"}
+
+fetchto = "../ip_cores"
